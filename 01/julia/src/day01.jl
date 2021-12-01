3afb62a5857ba5274bed0f142e18252ae399c04b
László Vaskó <1771332+vlaci@users.noreply.github.com>
2021-12-01 17:56:22 +0100

    day 1 - part 2 in Julia

--- 01/julia/src/day01.jl
+++ 01/julia/src/day01.jl
@@ -12,8 +12,15 @@ function depth(measurements)
     count(p -> p[2] - p[1] > 0, partition(measurements, 2, 1))
 end
 
+function depth(measurements, windowsize)
+    sums₁ = map(sum, partition(measurements, windowsize, 1))
+    sums₂ = map(sum, partition(measurements[2:end], windowsize, 1))
+    count(p -> p[2] - p[1] > 0, zip(sums₁, sums₂))
+end
+
 function run()
     measurements = read()
     println("The answer to the first part is $(depth(measurements))")
+    println("The answer to the first part is $(depth(measurements, 3))")
 end
 end
