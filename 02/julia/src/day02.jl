8b02b8abd44f5d48cf1f8da35141bb159fce9ebc
László Vaskó <1771332+vlaci@users.noreply.github.com>
2021-12-02 11:54:43 +0100

    day 2 - part 2 in Julia

--- 02/julia/src/day02.jl
+++ 02/julia/src/day02.jl
@@ -14,23 +14,43 @@ grammar = """
   %import common.NEWLINE
 """
 
-struct Course <: Transformer end
+parser = Lark(grammar, parser = "lalr")
 
-parser = Lark(grammar, parser="lalr", transformer=Course())
+mutable struct Direct <: Transformer end
+mutable struct Aimed <: Transformer
+    aim::Int
+    function Aimed()
+        new(0)
+    end
+end
+Course = Union{Direct,Aimed}
 
-@rule sum(s::Course, cmds) = reduce(.+, cmds)
+@rule sum(s::Course, cmds) = reduce(.+, cmds) |> prod
 @rule cmd(s::Course, cmd) = cmd[1]
-@rule dive(s::Course, amount) = (amount[1], 0)
-@rule ascent(s::Course, amount) = (-amount[1], 0)
-@rule forward(s::Course, amount) = (0, amount[1])
+@rule dive(s::Direct, amount) = (amount[1], 0)
+@rule ascent(s::Direct, amount) = (-amount[1], 0)
+@rule forward(s::Direct, amount) = (0, amount[1])
 @rule number(s::Course, nums) = parse(Int, nums[1])
 
-function dive(course)
-    Lerche.parse(parser, course) |> prod
+
+@rule dive(s::Aimed, amount) = begin
+    s.aim += amount[1]
+    (0, 0)
+end
+@rule ascent(s::Aimed, amount) = begin
+    s.aim -= amount[1]
+    (0, 0)
+end
+@rule forward(s::Aimed, amount) = (s.aim * amount[1], amount[1])
+
+function dive(course, transformer)
+    p = Lerche.parse(parser, course)
+    Lerche.transform(transformer(), p)
 end
 
 function run()
     course = read("$(@__DIR__)/../../input", String)
-    println("The answer to the first part is $(dive(course))")
+    println("The answer to the first part is $(dive(course, Direct))")
+    println("The answer to the second part is $(dive(course, Aimed))")
 end
 end
--- 02/julia/test/runtests.jl
+++ 02/julia/test/runtests.jl
@@ -9,5 +9,14 @@ using Test
     up 3
     down 8
     forward 2
-    """) == 150
+    """, day02.Direct) == 150
+
+    @test day02.dive("""
+    forward 5
+    down 5
+    forward 8
+    up 3
+    down 8
+    forward 2
+    """, day02.Aimed) == 900
 end
